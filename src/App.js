9bb9ef95ae29d98ca73b10803ac59cedc0b4fe99
Banji Oguntade <BOguntade@royalalliances.com>
2020-05-26 00:14:33 -0500

    error handling

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import React, {useEffect, useState} from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from './components/header/header';
 import NewsSources from './components/newsSources/newsSources';
-import Footer from './components/footer/footer';
 import NewsDetails from './components/newsDetails/newsDetails';
 import Filter from './components/filter/filter';
 import axios from "axios";
 import loading from './loading.gif';
+import errImage from './error.jpg';
 import './App.css';
 
 function App() {
   let [data, setData] = useState([]);
   let [filter, setFilter] = useState([]);
+  let [err, setErr] = useState(false);
   const [isLoading, setValue] = useState(true);
   const apiKey = '49e68def73af41b1927f24680bccc357';
 
@@ -21,6 +22,9 @@ function App() {
       .then(result => {
         setData(result.data.sources);
         setValue(false);
+      }).catch((err) => {
+        setValue(false);
+        setErr(true);
       });
   }, []);
 
@@ -32,18 +36,24 @@ function App() {
   return (
 
     <div className="App">
-      { (isLoading === true) ? <img src={loading} alt="" className="preloader" /> : null }
+      {
+         (isLoading === true) ? <img src={loading} alt="" className="preloader" /> : null
+      }
         <Router>
           <Route path="/:id" component={ NewsDetails } />
           <Route exact path="/">
-           <section style={{ marginBottom : "50px" }}>
-             <Header id="Sources" location="home" />
-             <Filter filterSources={filterSources} />
-             <NewsSources data={data} filter={filter} />
-           </section>
+          {
+            (err === true) ? <React.Fragment><img src={errImage} alt="" className="errImage" /> </React.Fragment> :
+              <React.Fragment>
+                <section style={{ marginBottom : "50px" }}>
+                  <Header id="Sources" location="home" />
+                  <Filter filterSources={filterSources} />
+                  <NewsSources data={data} filter={filter} />
+                </section>
+              </React.Fragment>
+          }
           </Route>
         </Router>
-        { (isLoading === false) ? <Footer /> : null}
     </div>
   );
 }
--- src/components/newsDetails/newsDetails.js
+++ src/components/newsDetails/newsDetails.js
@@ -2,12 +2,14 @@ import React,  {useEffect, useState} from 'react';
 import axios from "axios";
 import defaultImg from "./image-not-found.png"
 import loading from './loading.gif';
+import errImage from '../../error.jpg';
 import Header from '../header/header';
 import './newsDetails.css';
 
 export default function NewsDetails(props) {
     const [data, setData] = useState([]);
     const [isLoading, setValue] = useState(true);
+    let [err, setErr] = useState(false);
     const apiKey = '49e68def73af41b1927f24680bccc357';
     function checkImage(url){
         if ((url === null) || (url === "null") || (url === "")){
@@ -24,28 +26,35 @@ export default function NewsDetails(props) {
           .then((result) => {
             setData(result.data.articles)
             setValue(false)
+          }).catch((err) => {
+            setValue(false)
+            setErr(true)
           });
       }, [props]);
 
   return (
     <div className="newsDetails">
       <Header id={props.match.params.id} />
-      { (isLoading === true) ? <img src={loading} alt="" className="preloader" /> : null}
-      { data.map((item) => {
+      { (isLoading === true) ? <img src={loading} alt="" className="preloader" /> : null }
+      {  (err !== true) ? data.map((item) => {
           return (
-              <div className="newsDetails_item" key={item.publishedAt}>
-                  <div className="imgPlaceholder">
-                    <img src={checkImage(item.urlToImage)} alt="" />
-                  </div>
-                  <div className="content">
-                    <div className="newsDetails_title">{ item.title }</div>
-                    <div>{ item.description }</div>
-                    <div>{ item.content }</div>
-                    <div className="newsDetails_url"><strong><a href={ item.url }>{ item.url }</a></strong></div>
-                  </div>
-             </div>
+              <React.Fragment key={item.publishedAt}>
+                <div className="newsDetails_item">
+                    <div className="imgPlaceholder">
+                      <img src={checkImage(item.urlToImage)} alt="" />
+                    </div>
+                    <div className="content">
+                      <div className="newsDetails_title">{ item.title }</div>
+                      <div>{ item.description }</div>
+                      <div>{ item.content }</div>
+                      <div className="newsDetails_url"><strong><a href={ item.url }>{ item.url }</a></strong></div>
+                    </div>
+               </div>
+              </React.Fragment>
           )
-      }) }
+      }) : <React.Fragment><img src={errImage} alt="" className="errImage" /> </React.Fragment>
+
+      }
     </div>
   );
 }
